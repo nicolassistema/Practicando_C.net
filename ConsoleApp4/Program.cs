c1f791deabc337898a45d794d1ac020d01460767
nicolassistema <nicolas.letticugna@gmail.com>
2021-02-12 01:00:55 -0300

    Subiendo

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(calcular(10,5));
+
+     
+
+        }
+
+
+
+      static double calcular(int num1, double num2) {
+
+            double resultado;
+
+            resultado=num1 / num2;
+           
+            return resultado;
+        }
+
+     
+
+
+    }
+}
+    
