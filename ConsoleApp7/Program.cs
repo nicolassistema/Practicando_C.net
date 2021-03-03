8440e2f2ff3cbf7120f5b840d6f0aee7bf07d194
nicolassistema <nicolas.letticugna@gmail.com>
2021-03-02 23:15:07 -0300

    Subiendo

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int numero = int.Parse(Console.ReadLine());
+
+           // int resultado = numero % 2;
+
+            if ((numero % 2)==0)
+            {
+                Console.WriteLine("Es numero par");
+            }
+            else
+            {
+                Console.WriteLine("Es numero impar");
+            }
+
+
+
+        }
+    }
+}
