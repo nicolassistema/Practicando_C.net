d6c772d123d4fe42a14d6293e1c015e81bafe74c
nicolassistema <nicolas.letticugna@gmail.com>
2021-03-10 22:32:45 -0300

    Subiendo checked + try and catch con filtro 2

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -9,15 +9,15 @@ namespace ConsoleApp11
             //El desbordamiento no hace caer el programa por que visual studio tiene alto rendimiento y lo deja pasar 
             //la expresion "checked" no deja pasar el desbordamiento y hace caer el programa. 
             //Tambien se ppuede confirgurar para que siempre chequee "https://www.youtube.com/watch?v=vVRznsklCtI" en el minuto 17:39
-            checked
-            {
+           // checked
+           // {
                 int numero = int.MaxValue;
                 int resultado = numero + 20;
 
 
                 Console.WriteLine(resultado);
 
-            }
+            //}
 
 
 
